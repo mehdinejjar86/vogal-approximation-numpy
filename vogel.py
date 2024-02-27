383da3337db93a22cb7bdd14c3267d125b6d5441
Night^^Stalker <67825314+mehdinejjar86@users.noreply.github.com>
2024-02-27 13:01:55 +0100

    Update vogel.py

--- vogel.py
+++ vogel.py
@@ -10,8 +10,11 @@ def calculate_difference(cost_matrix):
     
     for i in range(len(row_difference)):
         indexes = cost_matrix[i].argsort()[:2]
-        if cost_matrix[i][indexes[0]] == np.inf or cost_matrix[i][indexes[1]] == np.inf:
-            row_difference[i] = -1
+        if cost_matrix[i][indexes[1]] == np.inf:
+            if cost_matrix[i][indexes[0]] == np.inf:
+                column_difference[i] = -1
+            else:
+                column_difference[i] = cost_matrix[i][indexes[0]]
         else:
             row_difference[i] = abs(cost_matrix[i][indexes[0]] - cost_matrix[i][indexes[1]])
 
@@ -19,8 +22,11 @@ def calculate_difference(cost_matrix):
     
     for i in range(len(column_difference)):
         indexes = cost_matrixT[i].argsort()[:2]
-        if cost_matrixT[i][indexes[0]] == np.inf or cost_matrixT[i][indexes[1]] == np.inf:
-            column_difference[i] = -1
+        if cost_matrixT[i][indexes[1]] == np.inf:
+            if cost_matrixT[i][indexes[0]] == np.inf:
+                column_difference[i] = -1
+            else:
+                column_difference[i] = cost_matrixT[i][indexes[0]]
         else:
             column_difference[i] = abs(cost_matrixT[i][indexes[0]] - cost_matrixT[i][indexes[1]])
     
@@ -33,42 +39,43 @@ def vogel_approximation_method(cost_matrix, supply, demand):
     while total_demand > result.sum():
     
         r, c = calculate_difference(cost_matrix=cost_matrix)
-        if c.max() >= r.max() and (c[0] != -1 or r[0] != -1):
+        print(r,c)
+        if c.max() >= r.max():
             column_index = c.argmax()
             
-            position_indexes = cost_matrix[:,column_index].argsort()
-            for position_index in position_indexes:
-                s = supply[position_index]
-                d = demand[column_index]
-                if s >= d:
-                    demand[column_index] -= d
-                    supply[position_index] -= d
-                    result[position_index, column_index] = d
-                    for j in range(len(r)):
-                        cost_matrix[j, column_index] = np.inf
-                    break
-                else:
-                    demand[column_index] -= s
-                    supply[position_index] -= s
-                    result[position_index, column_index] = s
+            position_index = cost_matrix[:,column_index].argmin()
+            s = supply[position_index]
+            d = demand[column_index]
+            if s >= d:
+                demand[column_index] -= d
+                supply[position_index] -= d
+                result[position_index, column_index] = d
+                for j in range(len(r)):
+                    cost_matrix[j, column_index] = np.inf
+            else:
+                demand[column_index] -= s
+                supply[position_index] -= s
+                result[position_index, column_index] = s
+                for j in range(len(c)):
+                    cost_matrix[position_index, j] = np.inf
         
         else:
             row_index = r.argmax()
             
-            position_indexes = cost_matrix[row_index].argsort()
-            for position_index in position_indexes:
-                s = supply[row_index]
-                d = demand[position_index]
-                if s >= d:
-                    demand[position_index] -= d
-                    supply[row_index] -= d
-                    result[row_index, position_index] = d
-                    for j in range(len(c)):
-                        cost_matrix[row_index, j] = np.inf
-                    break
-                else:
-                    demand[position_index] -= s
-                    supply[row_index] -= s
-                    result[row_index, position_index] = s
+            position_index = cost_matrix[row_index].argmin()
+            s = supply[row_index]
+            d = demand[position_index]
+            if s >= d:
+                demand[position_index] -= d
+                supply[row_index] -= d
+                result[row_index, position_index] = d
+                for j in range(len(r)):
+                    cost_matrix[j, position_index] = np.inf
+            else:
+                demand[position_index] -= s
+                supply[row_index] -= s
+                result[row_index, position_index] = s
+                for j in range(len(c)):
+                    cost_matrix[row_index, j] = np.inf
 
     return result
